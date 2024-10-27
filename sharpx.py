8896f1aff6ed52396f65143ce59a98a21d18d154
thorsaramod <thorsaramod@gmail.com>
2024-10-27 13:25:58 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY GOKU_VAMPIRE
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7048247456:AAEVEHsHrDf9b9TSmWNuderm-S_z7N3TFtY")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("GOKU_VAMPIRE")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1927767033")
+
+
+
+
+
+  
